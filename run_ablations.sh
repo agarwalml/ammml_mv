8557fef9a1ec2092be0183b7c2f9459d806e2d1c
Santiago Benoit <santib27@gmail.com>
2023-03-22 07:31:58 -0400

    debug ablations

--- run_ablations.sh
+++ run_ablations.sh
@@ -6,7 +6,7 @@ do
     do
         id=${ms}_${url: -11}
         echo $id
-        echo -e "#!/usr/bin/env bash\n\nconda activate mv\npython ablations.py $url /results/sbenoit/ammml_mv/ablation/$id >& $id.out\ncd /results/sbenoit/ammml_mv/ablation/$id\nconda activate ffmpeg\nffmpeg -f concat -i concat.txt -i audio.mp3 -map 0:v -map 1:a -c:v libx264 -vsync vfr -vf mpdecimate -pix_fmt yuv420p -c:a copy -shortest -y video.mp4\n" > $id.sh
+        echo -e "#!/usr/bin/env bash\n\nconda activate mv\npython ablations.py $ms $url /results/sbenoit/ammml_mv/ablation/$id >& $id.out\ncd /results/sbenoit/ammml_mv/ablation/$id\nconda activate ffmpeg\nffmpeg -f concat -i concat.txt -i audio.mp3 -map 0:v -map 1:a -c:v libx264 -vsync vfr -vf mpdecimate -pix_fmt yuv420p -c:a copy -shortest -y video.mp4\n" > $id.sh
         chmod +x $id.sh
         sbatch -p gpu_low --gres=gpu:1 --mem=16G -x compute-1-17 $id.sh
     done < urls.txt
